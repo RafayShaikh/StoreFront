07b0a50f5c5f673cc4b99497a7ce718803063d8d
RafayShaikh <rafaysarr@gmail.com>
2021-05-24 12:01:05 -0500

    Day 2

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyA2McvVY3Hh6Y8IYWFLWpTykWZiv3mAouM',
+  authDomain: 'store-front-template.firebaseapp.com',
+  projectId: 'store-front-template',
+  storageBucket: 'store-front-template.appspot.com',
+  messagingSenderId: '894379496994',
+  appId: '1:894379496994:web:4c00d97f60b4d347f1dc7b',
+};
